309fd54e41c150c350b0e2f1b4c195120bbc15ce
hulk510 <dorian.51069@gmail.com>
2022-12-03 15:21:36 +0900

    discordjs入れた nodeが16以上じゃないと動かないっぽいので19で設定

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
+import Discord, { GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
+
+const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on('ready', () => {
+  console.log('Ready...');
+});
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-(async () => {
-  const response = (
-    await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: 'やっほーこんにちは、今日の運勢を教えてください',
-      temperature: 0,
-      max_tokens: 512,
-    })
-  ).data.choices[0].text;
-  console.log(response);
-})();
+// (async () => {
+//   const response = (
+//     await openai.createCompletion({
+//       model: 'text-davinci-003',
+//       prompt: 'やっほーこんにちは、今日の運勢を教えてください',
+//       temperature: 0,
+//       max_tokens: 512,
+//     })
+//   ).data.choices[0].text;
+//   console.log(response);
+// })();
