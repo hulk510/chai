413688783e8e2b92c39682039e7e09decf6f38a0
hulk510 <dorian.51069@gmail.com>
2023-03-04 01:17:14 +0900

    gpt3.5を試せるように修正

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Discord, { Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
-import { Readable } from 'stream';
-import { getMessageStream } from './openai';
+import { getMessage } from './openai';
 
 dotenv.config();
 
@@ -20,50 +19,29 @@ client.on('ready', () => {
 
 client.on(Events.MessageCreate, async (message) => {
   if (message.author.bot) return;
+  // ここでチャンネルIDを動的に取得したい
   if (message.channelId !== '1081105541545328730') {
     return;
   }
 
-  // message.channel.isThread();
-
-  // const thread = await message.startThread({
-  //   name: message.content,
-  //   autoArchiveDuration: 60,
-  //   reason: 'Needed a separate thread for food',
-  // });
-  // console.log(`Created thread: ${thread.name}`);
-  // console.log(`Thread ID: ${thread.id}`);
-  const stream = (await getMessageStream(message.content)) as any as Readable;
-  let msg:
-    | Promise<Discord.Message<false>>
-    | Promise<Discord.Message<true>>
-    | null = null;
-  let responseMessage = '';
-  stream.on('data', (chunk) => {
-    try {
-      const chunkString = chunk.toString().trim();
-      if (chunkString.startsWith('data: ') || chunkString.endsWith('\n')) {
-        console.log(chunkString);
-        const res = chunkString.replace('data: ', '');
-        console.log('hey', res);
-        const data = JSON.parse(res);
-        const content = data.choices[0].delta?.content || '';
-        if (content.trim() === '') {
-          return;
-        }
-        if (!msg) {
-          responseMessage = content;
-          msg = message.channel.send(responseMessage);
-        } else {
-          responseMessage += content;
-          msg.then((m) => {
-            m.edit(responseMessage);
-          });
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      message.channel.send('返信を返せません');
-    }
-  });
+  let dots = '🤔 .';
+  let msg = message.channel.send(dots);
+  let interval = setInterval(() => {
+    dots += ' .';
+    msg.then((m) => {
+      m.edit(dots);
+    });
+  }, 1000);
+  try {
+    const response = await getMessage(message.content);
+    message.channel.send(response);
+  } catch (error) {
+    console.error(error);
+    message.channel.send('返信を返せません');
+  } finally {
+    clearInterval(interval);
+    msg.then((m) => {
+      m.delete();
+    });
+  }
 });
--- src/openai.ts
+++ src/openai.ts
@@ -1,19 +1,18 @@
 import { Configuration, OpenAIApi } from 'openai';
-// https://vercel.com/blog/gpt-3-app-next-js-vercel-edge-functions
-export const getMessageStream = async (message: string) => {
+
+export const getMessage = async (message: string) => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  const response = await openai.createChatCompletion(
-    {
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: message }],
-      temperature: 0,
-      stream: true,
-      max_tokens: 1024,
-    },
-    { responseType: 'stream' }
-  );
-  return response.data;
+  const response =
+    (
+      await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: message }],
+        temperature: 0,
+        max_tokens: 768,
+      })
+    ).data.choices[0].message?.content || '';
+  return response;
 };
