aac4eaca8ab14c33719b58a455adb76711a81dd2
hulk510 <dorian.51069@gmail.com>
2023-03-04 00:01:26 +0900

    編集して送れる（たまにパースエラーになる）ようになった。ただ遅い

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Discord, { Events, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
-import { getMessage } from './openai';
+import { Readable } from 'stream';
+import { getMessageStream } from './openai';
 
 dotenv.config();
 
@@ -18,7 +19,6 @@ client.on('ready', () => {
 });
 
 client.on(Events.MessageCreate, async (message) => {
-  console.log(message);
   if (message.author.bot) return;
   if (message.channelId !== '1081105541545328730') {
     return;
@@ -33,13 +33,37 @@ client.on(Events.MessageCreate, async (message) => {
   // });
   // console.log(`Created thread: ${thread.name}`);
   // console.log(`Thread ID: ${thread.id}`);
-  try {
-    const response = await getMessage(message.content);
-    message.channel.send(response);
-  } catch (error) {
-    console.error(error);
-    message.channel.send('返信を返せません');
-  }
+  const stream = (await getMessageStream(message.content)) as any as Readable;
+  let msg:
+    | Promise<Discord.Message<false>>
+    | Promise<Discord.Message<true>>
+    | null = null;
+  let responseMessage = '';
+  stream.on('data', (chunk) => {
+    try {
+      const chunkString = chunk.toString().trim();
+      if (chunkString.startsWith('data: ') || chunkString.endsWith('\n')) {
+        console.log(chunkString);
+        const res = chunkString.replace('data: ', '');
+        console.log('hey', res);
+        const data = JSON.parse(res);
+        const content = data.choices[0].delta?.content || '';
+        if (content.trim() === '') {
+          return;
+        }
+        if (!msg) {
+          responseMessage = content;
+          msg = message.channel.send(responseMessage);
+        } else {
+          responseMessage += content;
+          msg.then((m) => {
+            m.edit(responseMessage);
+          });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      message.channel.send('返信を返せません');
+    }
+  });
 });
-
-// client.on(Events.)
--- src/openai.ts
+++ src/openai.ts
@@ -1,7 +1,6 @@
 import { Configuration, OpenAIApi } from 'openai';
-import { Readable } from 'stream';
-
-export const getMessage = async (message: string): Promise<string> => {
+// https://vercel.com/blog/gpt-3-app-next-js-vercel-edge-functions
+export const getMessageStream = async (message: string) => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -16,53 +15,5 @@ export const getMessage = async (message: string): Promise<string> => {
     },
     { responseType: 'stream' }
   );
-  const stream = response.data as any as Readable;
-  let streamHead = true; // Flag to indicate whether a message begins the stream or is a continuation
-  let responseData = '';
-  return new Promise((resolve, reject) => {
-    stream.on('data', (chunk) => {
-      try {
-        const chunkString = chunk
-          .toString()
-          .trim()
-          .replace(/\\n/g, '\\n')
-          .replace(/\\'/g, "\\'")
-          .replace(/\\"/g, '\\"')
-          .replace(/\\&/g, '\\&')
-          .replace(/\\r/g, '\\r')
-          .replace(/\\t/g, '\\t')
-          .replace(/\\b/g, '\\b')
-          .replace(/\\f/g, '\\f');
-        console.log(chunkString);
-        if (chunkString.startsWith('data: ')) {
-          const res = chunkString.replace('data: ', '');
-          // if (res === '[DONE]') {
-          //   stream.resume();
-          //   return;
-          // }
-          const data = JSON.parse(res);
-          if (streamHead) {
-            responseData = data.choices[0].delta?.content || '';
-            streamHead = false;
-          } else {
-            responseData += data.choices[0].delta?.content || '';
-          }
-        }
-      } catch (error) {
-        // End the stream but do not send the error, as this is likely the DONE message from createCompletion
-        console.error(error);
-      }
-    });
-
-    // Send the end of the stream on stream end
-    stream.on('end', () => {
-      resolve(responseData);
-    });
-
-    // If an error is received from the completion stream, send an error message and end the response stream
-    stream.on('error', (error) => {
-      // console.error(error);
-      reject(error);
-    });
-  });
+  return response.data;
 };
