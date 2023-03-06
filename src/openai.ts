e9d00689639bc9a30902d1a5fdc6e781f9b2e365
hulk510 <dorian.51069@gmail.com>
2023-03-06 12:05:57 +0900

    promptTemplateテスト

--- src/openai.ts
+++ src/openai.ts
@@ -1,3 +1,4 @@
+import { PromptTemplate } from 'langchain';
 import { ConversationChain } from 'langchain/chains';
 import { OpenAIChat } from 'langchain/llms';
 import { BufferWindowMemory } from 'langchain/memory';
@@ -7,6 +8,7 @@ interface Memories {
   [key: string]: BufferWindowMemory;
 }
 export const memories: Memories = {};
+
 export const getLCMessage = async (
   message: string,
   memory: BufferWindowMemory
@@ -15,13 +17,28 @@ export const getLCMessage = async (
     modelName: 'gpt-3.5-turbo',
     openAIApiKey: process.env.OPENAI_API_KEY,
     temperature: 0.9,
+    prefixMessages: [
+      { role: 'system', content: '日本語で回答してください' },
+      { role: 'assistant', content: 'こんにちは' },
+      { role: 'user', content: '短くできる限りわかりやすく簡潔に' },
+    ],
   });
 
+  const prompt = new PromptTemplate({
+    template:
+      '{target}に関する、Twitterで人を惹きつける魅力的な文章を考えてください。',
+    inputVariables: ['target'],
+  });
+
+  // LLMChainのの違いは？
   const chain = new ConversationChain({
     llm: model,
     memory: memory,
+    prompt: prompt,
   });
-  const res = await chain.call({ input: message });
+  const res = await chain.call({
+    target: message,
+  });
   return res.response;
 };
 
